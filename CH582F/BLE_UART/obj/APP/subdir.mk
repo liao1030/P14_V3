751d7f5bd36ae776ca3bab7302b4b1c6b1ab0d8a
liao1030 <liao1030@gmail.com>
2025-07-30 21:30:35 +0800

    修正熱敏電阻表格錯誤
     static const TempResistancePair_TypeDef tempTable[] = {
            {-10.0, 277926.5},   // -10°C
            { -5.0, 212221.1},   //  -5°C
            {  0.0, 163500.0},   //   0°C
            {  5.0, 127036.5},   //   5°C
            { 10.0,  99504.5},   //  10°C
            { 15.0,  78540.0},   //  15°C
            { 20.0,  62447.6},   //  20°C
            { 25.0,  50000.0},   //  25°C
            { 30.0,  40301.0},   //  30°C
            { 35.0,  32690.9},   //  35°C
            { 37.0,  30117.4},   //  37°C (人體溫度)
            { 40.0,  26679.8},   //  40°C
            { 45.0,  21901.5},   //  45°C
            { 50.0,  18080.0},   //  50°C
        };

--- CH582F/BLE_UART/obj/APP/subdir.mk
+++ CH582F/BLE_UART/obj/APP/subdir.mk
@@ -1,5 +1,5 @@
 ################################################################################
-# MRS Version: 2.1.0
+# MRS Version: 2.2.0
 # Automatically-generated file. Do not edit!
 ################################################################################
 
@@ -23,7 +23,15 @@ OBJS += \
 ./APP/peripheral_main.o 
 
 
+EXPANDS += \
+./APP/StripDetect.c.234r.expand \
+./APP/app_uart.c.234r.expand \
+./APP/peripheral.c.234r.expand \
+./APP/peripheral_main.c.234r.expand 
+
+
 
 # Each subdirectory must supply rules for building sources it contributes
 APP/%.o: ../APP/%.c
 	@	riscv-none-embed-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -msmall-data-limit=8 -mno-save-restore -fmax-errors=20 -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -g -DDEBUG=2 -DCH582F -I"/Users/liao1030/HMD/_Projects/_P14_V2_1/Firmware/P14_V3/CH582F/BLE_UART/Startup" -I"/Users/liao1030/HMD/_Projects/_P14_V2_1/Firmware/P14_V3/CH582F/BLE_UART/APP/include" -I"/Users/liao1030/HMD/_Projects/_P14_V2_1/Firmware/P14_V3/CH582F/BLE_UART/Profile/include" -I"/Users/liao1030/HMD/_Projects/_P14_V2_1/Firmware/P14_V3/CH582F/BLE_UART/StdPeriphDriver/inc" -I"/Users/liao1030/HMD/_Projects/_P14_V2_1/Firmware/P14_V3/CH582F/BLE_UART/HAL/include" -I"/Users/liao1030/HMD/_Projects/_P14_V2_1/Firmware/P14_V3/CH582F/BLE_UART/Ld" -I"/Users/liao1030/HMD/_Projects/_P14_V2_1/Firmware/P14_V3/CH582F/BLE_UART/LIB" -I"/Users/liao1030/HMD/_Projects/_P14_V2_1/Firmware/P14_V3/CH582F/BLE_UART/RVMSIS" -I"/Users/liao1030/HMD/_Projects/_P14_V2_1/Firmware/P14_V3/CH582F/BLE_UART/APP/app_drv_fifo" -I"/Users/liao1030/HMD/_Projects/_P14_V2_1/Firmware/P14_V3/CH582F/BLE_UART/APP/ble_uart_service" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+
